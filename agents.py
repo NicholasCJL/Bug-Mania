00775d09b772249870148f3b5efa6999c9d55773
Nicholas C <chong.jiale.nicholas@gmail.com>
2022-09-21 21:46:06 +0800

    Test neural network with random inputs

--- agents.py
+++ agents.py
@@ -1,137 +1,119 @@
 import pygame
-from objects import Rect, Food
+from sprites import BugSprite
+from NeuralNetwork import BugNN
+import numpy as np
 import random
-from utility import Direction, manhattan_distance
+from utility import Vector, Point
+from utility import euclidean_distance as distance
 
-class Ant:
-    def __init__(self, x, y, max_speed, max_energy, bounds, colour=(0, 255, 0)):
-        self.location = (x, y)
+class Bug:
+    max_eyesight = 100
+    food_energy = 50
+    movement_cost = 0.1
+    rotation_cost = 0.01
+    wall_collide_cost = 1
+    bug_collide_cost = 1
+    eat_cost = 0.2
+    eaten_cost = 0.1 # proportion of eater
+    eat_energy = 0.1 # proportion of eater
+
+    def __init__(self, position, max_speed, max_energy, max_rotate,
+                 bounds, body_colour, leg_colour,
+                 horn_colour, size, fov, eyesight, food_energy=None, movement_cost=None,
+                 rotation_cost=None, wall_collide_cost=None,
+                 bug_collide_cost=None, eat_cost=None, eaten_cost=None,
+                 eat_energy=None, nn_seed=22):
+        # self.curr_angle = 0
         self.max_speed = max_speed
         self.max_energy = max_energy
-        self.bounds = bounds
-        self.colour = colour
+        self.max_rotate = max_rotate
+        self.energy = max_energy
+        self.x_bound, self.y_bound = bounds # x and y value indicating width and height of field
+        self.body_colour, self.leg_colour, self.horn_colour = body_colour, leg_colour, horn_colour
+        self.size = size
+        self.fov = [i * fov / 6 for i in range(-3, 4, 2)]
+        self.eyesight = eyesight
+        self.food_energy = food_energy if food_energy is not None else Bug.food_energy
+        self.movement_cost = movement_cost if movement_cost is not None else Bug.movement_cost
+        self.rotation_cost = rotation_cost if rotation_cost is not None else Bug.rotation_cost
+        self.wall_collide_cost = wall_collide_cost if wall_collide_cost is not None else Bug.wall_collide_cost
+        self.bug_collide_cost = bug_collide_cost if bug_collide_cost is not None else Bug.bug_collide_cost
+        self.eat_cost = eat_cost if eat_cost is not None else Bug.eat_cost
+        self.eaten_cost = eaten_cost if eaten_cost is not None else Bug.eaten_cost
+        self.eat_energy = eat_energy if eat_energy is not None else Bug.eat_energy
+        self.sprite = BugSprite(position, body_colour=self.body_colour, horn_colour=self.horn_colour,
+                                leg_colour=self.leg_colour)
+        self.brain = BugNN(seed=nn_seed)
 
-
-
-class Ant2:
-    MOVEMENT_OPTIONS = ([0, 1], [0, -1], [1, 0], [-1, 0])
-    MOVEMENT_DICT = {Direction.DOWN: [0, 1],
-                     Direction.UP: [0, -1],
-                     Direction.RIGHT: [1, 0],
-                     Direction.LEFT: [-1, 0]}
-
-    def __init__(self, energy, x, y, colour, bounds,
-                 min_energy, rest_energy, coarse_size, fine_radius,
-                 width=1, height=1):
-        self.energy = energy
-        self.sprite = Rect(width, height, x, y, colour, [0, 0], bounds)
-        self.is_resting = False
-        self.min_energy = min_energy
-        self.rest_energy = rest_energy
-        self.prev_dir = random.choice(Ant.MOVEMENT_OPTIONS)
-        self.coarse_size = coarse_size
-        self.coarse_bound = coarse_size // 3
-        self.fine_radius = fine_radius
-
-    def act(self, food_state):
-        # check tiredness
-        if self.energy < self.min_energy:
-            self.energy += 1
-            self.is_resting = True
+    def update(self, state):
+        if self.energy <= 0:
             return
-        if self.is_resting and self.energy < self.rest_energy:
-            self.energy += 1
-            return
-        if self.energy >= self.rest_energy:
-            self.is_resting = False
-        moved = False
 
-        # look for food
+        # generate fake random input
+        nn_input = 2 * np.random.random(6) - 1
 
-        # move randomly
-        possible_movements = random.sample(Ant.MOVEMENT_OPTIONS, 4)
-        for movement in possible_movements:
-            x_bound, y_bound = self.sprite.is_move_within_bounds(movement)
-            if x_bound and y_bound:
-                self.sprite.move_by(*movement)
-                moved = True
-                self.energy -= 1
-                break
-        if not moved:
-            self.energy += 1
+        # get output of neural network
+        nn_output = self.brain.forward(nn_input)
 
-    def look_for_food(self, food_state: list[Food]):
-        # coarse search
-        search_area, dir, bounds = self._coarse_search_area()
-        target = self._coarse_search_contains(search_area, food_state)
-        if target is not None:
+        # choose action
+        self._get_action(nn_output)
 
-    def _get_coarse_direction(self, target, dir, bounds) -> list[int, int]:
-        # write method to convert relative to absolute directions
-        tx, ty = target.get_pygame_Rect().center
-        lower_bound, upper_bound = bounds
-        match dir:
-            case Direction.UP:
-                if tx < lower_bound:
-                    return Ant.MOVEMENT_DICT[Direction.LEFT]
-                if tx < upper_bound:
-                    return Ant.MOVEMENT_DICT[Direction.UP]
-                else:
-                    return Ant.MOVEMENT_DICT[Direction.RIGHT]
-            case Direction.DOWN:
-                if tx < lower_bound:
-                    return Ant.MOVEMENT_DICT[Direction.RIGHT]
-                if tx < upper_bound:
-                    return Ant.MOVEMENT_DICT[Direction.DOWN]
-                else:
-                    return Ant.MOVEMENT_DICT[Direction.LEFT]
-            case Direction.LEFT:
-                if ty < lower_bound:
-                    return Ant.MOVEMENT_DICT[Direction.DOWN]
+        # # convert game state to BugNN input
+        # nn_input = self.get_input(state)
+        # # get neural network output
+        # nn_output = self.brain.forward(nn_input)
+        # # perform action based on output
+        # action, energy_change = self.get_action(nn_output, state)
+        # # if action damages another bug / destroys food update state
+        # if action == "something":
+        #     self.update_state(state)
+        # # updates attributes
+        # self.update_self(action, energy_change)
 
+    def get_input(self, state):
+        energy = self.energy
 
-    def _coarse_search_contains(self, search_area, food_state: list[Food]) -> Food:
-        food_state_rects = [food.get_pygame_Rect() for food in food_state]
-        candidate_indices = search_area.collidelistall(food_state_rects)
-        if len(candidate_indices) == 0:
-            return None
-        curr_loc = self.sprite.get_loc()
-        candidate_food_states = [(manhattan_distance(curr_loc, food_state_rects[index].center),
-                                  food_state[index])
-                                 for index in candidate_indices]
-        candidate_food_states.sort(key=lambda x:x[0])
-        return candidate_food_states[0][1]
+    def _get_action(self, nn_output):
+        action = np.argmax(nn_output[:3])
+        # print(action, nn_output[:3])
+        # rotation
+        if action == 0:
+            self.rotate(self.max_rotate)
+            self.energy -= Bug.rotation_cost
+        elif action == 1:
+            self.rotate(-self.max_rotate)
+            self.energy -= Bug.rotation_cost
 
-    def _coarse_search_area(self) -> tuple[pygame.Rect, int, tuple[int, int]]:
-        # define search area
-        curr_loc_x, curr_loc_y = self.sprite.get_loc()
-        search_left, search_top, search_width, search_height = 0, 0, 0, 0
-        dir = None
-        lower_bound, upper_bound = 0, 0
-        if self.prev_dir[0] == 0: # x-axis zero
-            dir = Direction.UP if self.prev_dir[1] == -1 else Direction.DOWN
-            search_left = curr_loc_x - self.coarse_size
-            search_width = self.coarse_size * 2
-            search_top = curr_loc_y - self.coarse_size if self.prev_dir[1] == -1 \
-                else curr_loc_y
-            search_height = self.coarse_size
-            lower_bound, upper_bound = curr_loc_x - self.coarse_bound, curr_loc_x + self.coarse_bound
-        else: # y-axis zero
-            dir = Direction.LEFT if self.prev_dir[0] == -1 else Direction.RIGHT
-            search_top = curr_loc_y - self.coarse_size
-            search_height = self.coarse_size * 2
-            search_left = curr_loc_x - self.coarse_size if self.prev_dir[0] == -1 \
-                else curr_loc_x
-            search_width = self.coarse_size
-            lower_bound, upper_bound = curr_loc_y - self.coarse_bound, curr_loc_y + self.coarse_bound
-        return (pygame.Rect(search_left, search_top, search_width, search_height),
-                dir, (lower_bound, upper_bound))
+        # move in straight line
+        direction = self.sprite.pointing_vector()
+        move_vector = direction * (self.max_speed * nn_output[3])
+        destination = self.sprite.translate_by(move_vector)
+        if (0 < destination.x < self.x_bound) and (0 < destination.y < self.y_bound):
+            self.sprite.translate_by_ip(direction * (self.max_speed * nn_output[3]))
+            self.energy -= Bug.movement_cost
+        else:
+            self.energy -= Bug.wall_collide_cost
+
+        if self.energy <= 0:
+            pass
+            self.sprite.new_colour(body_colour=(80, 80, 80, 150),
+                                   leg_colour=(80, 80, 80, 150),
+                                   horn_colour=(80, 80, 80, 150))
+
+    def get_action(self, nn_output, state):
+        pass
+
+    def update_state(self, state):
+        pass
+
+    def update_self(self, action, energy_change):
+        pass
 
     def draw(self):
-        return self.sprite.draw(), self.sprite.get_loc()
+        return self.sprite.draw()
 
-    def to_draw(self):
-        return self.sprite.to_draw
+    def get_coords(self):
+        return self.sprite.get_coords()
 
-    def get_energy(self):
-        return self.energy
+    def rotate(self, angle):
+        self.sprite.rotate_about_midpoint_ip(angle)
